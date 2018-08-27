d9bbae8ad3a06ef124fdd7ea005f907b29d510af
edwinpaye <edwinpaye@gmail.com>
2018-08-26 20:32:48 -0400

    arreglo de ConetionSql

--- UserManager/src/main/java/com/UserManager/ConectionSql.java
+++ UserManager/src/main/java/com/UserManager/ConectionSql.java
@@ -2,39 +2,25 @@ package com.UserManager;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 
 public class ConectionSql {
-    private String user,password;
-    private Connection myConnection = null;
+    private String path, user,password;
     
-    public ConectionSql(String user,String password) {
+    public ConectionSql(String path,String user,String password) {
+        this.path=path;
         this.user=user;
         this.password = password;
     }
     
-    public boolean isConnected() {
+    public Connection Connect() {
         try{
-            System.out.println("Checking data");
-            if (Connect().isClosed()) {
-                System.out.println("The database is turned off");
-                return false;
-            }
+
+            return DriverManager.getConnection(path,this.user,this.password);
         }catch (Exception e) {
             System.out.println("Unable to establish connection " + e.getMessage());
             e.getStackTrace();
         }
-        return true;
+        return null;
     }
-    
-    public Connection Connect() {
-        try {
-            System.out.println("Stable connection");
-            return myConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/USUARIO",this.user,this.password);
-        }catch (Exception e) {
-            System.out.println("Connection error");
-        }
-        return myConnection;
-    }
+//
 }
